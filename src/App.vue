a506b1c2e634bcc65648c611c86f378e57695e40
iba_ragi <[ib.yt.56@icloud.com]>
2018-10-27 20:58:22 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/kamome.png">
+    <HelloWorld msg="Welcome to MyTestSite"/>
   </div>
 </template>
 
